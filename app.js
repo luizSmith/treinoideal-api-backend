fe4e7d8ce52ee19949ecd5235368894ce5a1eb6b
Luiz Victor <luiz.victor100@hotmail.com>
2020-05-18 19:24:27 -0300

    Iniciando Estrutura

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const session = require("express-session");
 
+//Body parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// Router
 app.get("/",function(req,resp){
      console.log("Aplicação rodando com sucesso");
      resp.send("Hello World!");
