5c267a1e00d9b44231b31bee5c847a02c1d10aee
Luiz Victor <luiz.victor100@hotmail.com>
2020-05-19 22:51:32 -0300

    Model Migration aparelho

---
+++ models/aparelho.js
@@ -0,0 +1,38 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Aparelho = sequelize.define('Aparelho', {
+    cd_aparelho: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    nm_aparelho: {
+      type:DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        notEmpty: {
+          msg: "Nome do aparelho Não pode ser nulo"
+        }
+      }
+    }, 
+    ds_aparelho: {
+      type:DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: "Descrição Não pode ser nula"
+        }
+      }
+    } 
+  }, {});
+  Aparelho.associate = function(models) {
+    // associations can be defined here
+    Aparelho.hasMany(models.Exercicio, {
+      foreignKey: 'cd_aparelho',
+      onDelete: 'CASCADE'
+    });
+  };
+  return Aparelho;
+};
