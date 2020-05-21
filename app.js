3812e304077f4f277d165972e62d965d54085b28
Luiz Victor <luiz.victor100@hotmail.com>
2020-05-21 19:40:06 -0300

    teste de cabeçalho

--- app.js
+++ app.js
@@ -32,13 +32,13 @@ app.use(function (req, resp, next) {
 });
 
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    // Request headers you wish to allow
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
