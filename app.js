ebd57be109d587bf71b7b4c489c7ad8a748643be
Luiz Victor <luiz.victor100@hotmail.com>
2020-05-21 20:10:12 -0300

    Cabeçalho resolvido

--- app.js
+++ app.js
@@ -14,6 +14,14 @@ const CepRouter = require("./routes/CepRouter");
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+app.use(function (req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 //Routes
 app.use("/",PersonalRouter);
 app.use("/",AparelhoRouter);
@@ -31,20 +39,7 @@ app.use(function (req, resp, next) {
     resp.status(404).render("notFound");
 });
 
-app.use(function (req, res, next) {
-    
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    // Request headers you wish to allow
-    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
 
 app.listen(process.env.PORT || 3000,function(erro){
     if (erro) {
@@ -52,5 +47,5 @@ app.listen(process.env.PORT || 3000,function(erro){
     } else {
         console.log("servidor rodando com sucesso");
     }
-    console.log("servidor rodando com sucesso 2");
+    console.log("servidor rodando com sucesso 23");
 });
