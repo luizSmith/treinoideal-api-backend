cfd996dfbb804f205d8084eb56a762ffe612e439
Luiz Victor <luiz.victor100@hotmail.com>
2020-05-31 19:52:22 -0300

    Model Aula

---
+++ models/aula.js
@@ -0,0 +1,47 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Aula = sequelize.define('tb_aula', {
+    cd_aula: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    dt_aula: {
+      type:DataTypes.DATE,
+      allowNull: false,
+      validate: {
+          isDate:{
+              msg: "Tem que ser uma data valida"
+          },
+          notEmpty: {
+            msg: "Data Aula Não pode ser nula"
+          }
+      }
+    },
+    ic_feito:{
+        type:DataTypes.INTEGER(1),
+        validate: {
+            isIn: [['0','1']]
+        }
+    },
+    cd_horario: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: "Código do horario Não pode ser nulo"
+        }
+      }
+    }
+  }, {});
+  Aula.associate = function(models) {
+    // associations can be defined here
+    Aula.belongsTo(models.tb_horario,{
+      foreignKey: 'cd_horario',
+      onDelete: "CASCADE",
+      onUpdate: "CASCADE"
+    })
+  };
+  return Aula;
+};
