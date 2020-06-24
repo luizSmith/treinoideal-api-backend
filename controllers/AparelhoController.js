538826b0e24d271998004057854f1148d44086c7
Luiz Victor <luiz.victor100@hotmail.com>
2020-06-23 22:02:12 -0300

    Aparelho: Tratamento de erros e limpeza

--- controllers/AparelhoController.js
+++ controllers/AparelhoController.js
@@ -5,28 +5,22 @@ class AparelhoController {
     async index(req, res) {
         try {
             let result = await AparelhoService.lista();
-            res.statusCode = 200;
-            res.json(result);
+            res.status(200).json(result);
         } catch (err) {
-            res.statusCode = 400;
-            res.json({erro:err})
+            res.status(400).json(err);
         }
     }
 
     async detals(req, res) {
         let id = req.params.id;
 
-        if (isNaN(id)) {
-            res.statusCode = 404;
-            return res.send("Not Found");
-        }
-
         try {
+            await ResponseValidation.validaNumber(id,res);
+            
             let result = await AparelhoService.detalhes(id);
             ResponseValidation.detalhes(result,res);
         } catch (err) {
-            res.statusCode = 400;
-            res.json({erro:err})
+            res.status(400).json(err);
         }
     }
 
